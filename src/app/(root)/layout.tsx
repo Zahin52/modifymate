ff2dbaae1bf458911c6b013eb755e1e28814a5e1
Md Zahin Uddin <zahinuddin5@gmail.com>
2024-07-12 22:39:13 +0600

    sidebar and sidenav bar implemented with signin and signout layout looks

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Sidebar from "../../../components/shared/Sidebar";
+import Sidebar from "../../components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
       <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,12 @@
-import { UserButton } from '@clerk/nextjs';
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 function Home() {
   return (
     <div>
       <p>This is the Home</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   );
 }
 
-export default Home
+export default Home;
