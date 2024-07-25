ecf6b658ea13e3ba95e7bbc257362b2f3c40aeac
Md Zahin Uddin <zahinuddin5@gmail.com>
2024-07-26 03:17:00 +0600

    transition pages form added with zod validation

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForms from "@/components/shared/TransformationForms";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-function AddTransformationTypePage() {
+async function AddTransformationTypePage({
+  params: { type },
+}: SearchParamProps) {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
   return (
-    <div>AddTransformationTypePage</div>
-  )
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForms
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 }
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
