f9161f5d02279b62994269af30a6cbd52395710f
Md Zahin Uddin <zahinuddin5@gmail.com>
2024-07-27 04:18:07 +0600

    upload and transform field are added

--- src/components/shared/TransformationForms.tsx
+++ src/components/shared/TransformationForms.tsx
@@ -32,6 +32,7 @@ import { CustomField } from "./CustomField";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -225,6 +226,15 @@ function TransformationForms({
                 type={type}
               />
             )}
+          />
+
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
           />
         </div>
 
